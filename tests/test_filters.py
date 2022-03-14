f02a9ae1cf31175eb08e391535632bea3a45760a
Anthony <acgardner95@gmail.com>
2022-03-14 16:42:41 -0500

    initializing filter class

---
+++ tests/test_filters.py
@@ -0,0 +1,17 @@
+from plntter.src.utils.filters import AEKF, MEKF, UKF
+
+
+def test_AEKF():
+    aekf = AEKF()
+    pred = aekf.predict()
+    updt = aekf.update()
+
+def test_MEKF():
+    mekf = MEKF()
+    pred = mekf.predict()
+    updt = mekf.update()
+
+def test_UKF():
+    ukf = UKF()
+    pred = ukf.predict()
+    updt = ukf.update()
