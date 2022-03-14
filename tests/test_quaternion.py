2f78f19fa40c6b136ef7bf53dcc98ac596a049ad
Anthony <acgardner95@gmail.com>
2022-03-14 16:36:32 -0500

    added quaternion asserts

--- tests/test_quaternion.py
+++ tests/test_quaternion.py
@@ -3,13 +3,16 @@ import numpy as np
 from plntter.src.utils.quaternion import Quaternion
 
 
-def test_quat_setter():
+def test_quat_setters():
     q = [0,0,0,1]
-    qv = [0,0,0]
-    q0 = 1
+    qv = q[0:3]
+    q0 = q[-1]
     quat = Quaternion(q)
     assert quat.val == q
     assert quat.qv == qv
+    assert quat.qx == qv[0]
+    assert quat.qy == qv[1]
+    assert quat.qz == qv[2]
     assert quat.q0 == q0
 
 def test_quat_mult():
